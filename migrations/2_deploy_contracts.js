ecdf9235ab4cfd1251e7d94d2a1e2e542c2076fb
José Coelho <me@josecoelho.com>
2017-06-28 14:05:48 +1200

    Cleanup example contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var MessageStream = artifacts.require("./MessageStream.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(MessageStream);
 };
